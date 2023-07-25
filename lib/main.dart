933b080b39cc7915b1c22b720bab974bb20944cc
gh3ma <gaes.sy.2000@gmail.com>
2023-07-25 21:53:17 +0300

    Add some functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 
@@ -7,7 +8,7 @@ import 'package:flutter/material.dart';
       backgroundColor: Color.fromARGB(255, 107, 96, 86),
       appBar: AppBar(
         title: const Text(
-          'Maching game',
+          'Dice game',
           style: TextStyle(
             fontSize: 30,
             fontWeight:FontWeight.bold,
@@ -22,22 +23,77 @@ import 'package:flutter/material.dart';
   );
   }
 
-class ImagePage extends StatelessWidget {
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Column(
+  State<ImagePage> createState() => _ImagePageState();
+}
 
+class _ImagePageState extends State<ImagePage> {
+  
+ var leftImageNumber = 5;
+ var rightImageNumber = 3;
+
+ void changeImage(){
+   leftImageNumber = Random().nextInt(6) + 1 ; 
+   rightImageNumber = Random().nextInt(6) + 1 ;
+
+ }
+  @override
+  Widget build(BuildContext context) {
+
+
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-          'The first game',
-          style: 
-          TextStyle(
-            fontSize: 24,
-          ),
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Text(
+            leftImageNumber == rightImageNumber ? 
+            'You win' : 'Try it again',
+            style: 
+            TextStyle(
+              fontSize: 34,
+              color: Colors.white,
+            ),
+            ),
+        ),
+          Row(
+            children: [
+
+            Expanded(
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    changeImage();
+                  });
+                },
+                child: Image.asset(
+                  'images/Dice$leftImageNumber.png',
+                  ),
+              ),
+              ),
+              
+              Expanded(
+              child: TextButton(
+                onPressed: (){
+
+                  setState(() {
+                  changeImage();
+                });
+                },
+                child: Image.asset(
+                  'images/Dice$rightImageNumber.png',
+                  ),
+              ),
+              ),
+            ],
           ),
 
       ],
     );
   }
+
 }
